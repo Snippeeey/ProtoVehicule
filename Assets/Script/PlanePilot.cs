5dd0030362cc97ed3f367d1d24b9106bfba45d3d
Snippeeey <106236641+Snippeeey@users.noreply.github.com>
2022-09-22 14:52:07 +0200

    proto++

--- Assets/Script/PlanePilot.cs
+++ Assets/Script/PlanePilot.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlanePilot : MonoBehaviour
 {
+    public float speed;
+    public float accéleration;
+    public float decélerationpower;
+    public float camBias = 0.96f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,40 @@ public class PlanePilot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        rotation();
+        movement();
+        CamMovement();
+        groundcolliding();
+    }
+    private void rotation()
+    {
+       
+        transform.Rotate(Input.GetAxis("Vertical"), 0.0f, -Input.GetAxis("Horizontal"));
+    }
+    private void movement()
+    {
+        transform.position += transform.forward * Time.deltaTime * speed;
+        speed -= transform.forward.y*Time.deltaTime*decélerationpower;
+        if (speed<=35)
+        {
+            speed = 35;
+        }
+    }
+    private void CamMovement()
+    {
+        Vector3 moveCamTo = transform.position - transform.forward* 50f + Vector3.up * 35f;
+        Camera.main.transform.position = Camera.main.transform.position * camBias + moveCamTo*(1.0f - camBias); ;
+
+        Camera.main.transform.LookAt(transform.position+ transform.forward*30f);
+    }
+    private void groundcolliding()
+    {
+        float terrainHeightWhereWeAre = Terrain.activeTerrain.SampleHeight(transform.position);
+        if (terrainHeightWhereWeAre > transform.position.y)
+        {
+
+        }
         
     }
+
 }
